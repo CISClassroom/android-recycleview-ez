02e274774c8971088a4cd56a9341e45404c54107
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-15 16:38:57 +0700

    Update MainActivity.kt

--- app/src/main/java/com/cislab/recycleview/MainActivity.kt
+++ app/src/main/java/com/cislab/recycleview/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         val users = ArrayList<User>()
         users.add(User("suay","Mr.",R.mipmap.ic_launcher))
-        users.add(User("ต้นตาล","วรรณพงษ์ ภัททิยไพบูลย์",R.drawable.tontan))
+        users.add(User("Tontan","Mr.Wannaphong Phatthiyaphaibun",R.drawable.tontan))
         users.add(User("suay","Mr.",R.mipmap.ic_launcher))
         users.add(User("Panat Kum-in","Mr.",R.drawable.phai))
         users.add(User("suay","Mr.",R.mipmap.ic_launcher))
