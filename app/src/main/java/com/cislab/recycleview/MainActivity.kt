bac221e0428345d03e3364c438447dd73444923e
Marksolution <33540488+Marksolution@users.noreply.github.com>
2020-01-15 16:47:22 +0700

    Update MainActivity.kt

--- app/src/main/java/com/cislab/recycleview/MainActivity.kt
+++ app/src/main/java/com/cislab/recycleview/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         val users = ArrayList<User>()
         users.add(User("suay","Mr.",R.mipmap.ic_launcher))
         users.add(User("Wannaphong Phatthiyaphaibun","Mr.",R.drawable.tontan))
-        users.add(User("MARK","Mr.NARUBATE PRAROD",R.mipmap.ic_launcher))
+        users.add(User("NARUBATE PRAROD","Mr.",R.mipmap.ic_launcher))
         users.add(User("Panat Kum-in","Mr.",R.drawable.phai))
         users.add(User("suay","Mr.",R.mipmap.ic_launcher))
 
